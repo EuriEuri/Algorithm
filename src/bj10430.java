8bf084caf314b1067939a1ff77e4da4ba0cbadfb
euri <askwhy0919@naver.com>
2022-02-16 16:17:48 +0900

    baekjoon

---
+++ src/bj10430.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class bj10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B, C;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        if ((A >= 2 && A <= 10000) && (B >= 2 && B <= 10000) && (C >= 2 && C <= 10000)) {
+
+                    System.out.println((A + B) % C);
+                    System.out.println(((A % C) + (B % C)) % C);
+                    System.out.println((A * B) % C);
+                    System.out.println(((A % C) * (B % C)) % C);
+
+
+        } else return;
+    }
+}
