30987c8b9b10db9780a0f55539f720a63d0d377d
euri <askwhy0919@naver.com>
2022-02-16 16:47:11 +0900

    baekjoon

--- src/bj2588.java
+++ src/bj2588.java
@@ -8,7 +8,7 @@ public class bj2588 {
         System.out.println(a*(b%10));
         System.out.println(a*(b%100/10));
         System.out.println(a*(b%1000/100));
-        System.out.println( (a*(b%10)) + ((a*(b%100/10))*10) + ((a*(b%1000/100))*100) );
+        System.out.println(a*b);
     }
 }
 /*
