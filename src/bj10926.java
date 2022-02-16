78dbce9ecbe61a5bf10fb086a9cbf13e01ce967c
euri <askwhy0919@naver.com>
2022-02-16 16:17:55 +0900

    baekjoon

---
+++ src/bj10926.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/*
+public class bj10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id;
+        String str = "joonas";
+        id = sc.next();
+        if (id.equals(str)) {
+            System.out.println(id + "??!");
+        }
+        else System.out.println(id);
+    }
+}
+*/
+public class bj10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id + "??!");
+    }
+}
+
+/*
+if (temp.length() > 50) {
+    return;
+}
+ */
