e4976a59f61af4c4c6aec0f1fd05c2292bbc374e
euri <askwhy0919@naver.com>
2022-02-18 15:51:28 +0900

    baekjoon

---
+++ src/bj2743.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class bj2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(sc.next().length());
+    }
+}
+/*
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String eng = sc.next();
+        if (eng.length() < 100) {
+            System.out.println(eng.length());
+        }
+        else return;
+    }
+}
+ */
